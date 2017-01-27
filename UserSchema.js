c3fbe7a8fce421c20eb214da72685fd4064fc019
Brendan O'Keefe <brndnokf@yahoo.com>
2017-01-27 07:40:02 -0700

    not sure we ar going this way

--- UserSchema.js
+++ UserSchema.js
@@ -2,9 +2,11 @@
 
 module.exports = (mongoose) => {
    var UserSchema = new mongoose.Schema({ // creates a new mongoose schema called UserSchema
-      name: String,
+      first: String,
+      last: String,
       email: String,
-      password: String
+      zip: Number,
+      interests: Object
    });
 
    var User = mongoose.model('User', UserSchema); // create a new model called 'User' based on 'UserSchema'
---
+++ commands.js
@@ -0,0 +1,12 @@
+//server side
+
+db.createUser(
+   {
+      first: "String",
+      last: "String",
+      email: "String",
+      password: "String"
+      zip: "Number",
+      interests: {}
+   }
+);
